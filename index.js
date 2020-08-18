e0f9f3471559f9a3b05b9d5ff99c572522e80f3b
Asma Kolaib <as.kolaib@gmail.com>
2020-08-18 07:19:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,21 @@ function mapToSquare(sourceArray) {
 function reduceToTotal(sourceArray, startingPoint = 0){ 
   return sourceArray.reduce((acc ,cur ) => acc + cur , startingPoint );
 }
+function reduceToAllTrue(src) {
+  src.reduce((acc) => {
+    if(!acc) {return false
+  }
+  return true
+  })
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
 
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
