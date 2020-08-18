2bb2dd45f84a589194d92e4f62a10bb4776c1315
Asma Kolaib <as.kolaib@gmail.com>
2020-08-18 07:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  return sourceArray.map(a => a * -1);
+}
+function mapToNoChange(sourceArray) {
+  return sourceArray.map(a => a );
+}
+function mapToDouble(sourceArray) {
+  return sourceArray.map(a => a * 2 );
+}
+function mapToSquare(sourceArray) {
+  return sourceArray.map(a => a *  a);
+}
+function reduceToTotal(sourceArray, startingPoint = 0){ 
+  return sourceArray.reduce((acc ,cur ) => acc + cur , startingPoint );
+}
+function reduceToAllTrue(sourceArray) {
+  return sourceArray.reduce((acc ) => {if(!acc) return false ;return true
+  }, true); 
+}
+
+function reduceToAnyTrue(sourceArray) {
+  return sourceArray.reduce((acc ) => {if(!acc) return false ; return false
+  }); 
+}
